b28daddcf6bb609a96de955ca735ae1fe53caa75
Abderrahmen <abderrahmen@monprepa.tn>
2018-07-18 13:04:25 +0200

    Fixing Bootstrap Dialog problem

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -17,11 +17,11 @@ Rails.application.config.assets.precompile += %w( popper.min.js bootstrap_elegen
  jquery-2.2.4.min.js bootstrap.min.js wow.min.js waypoints.min.js owl.carousel.min.js 
  nivo-lightbox.min.js modernizr.custom.js jquery.validate.min.js jquery.counterup.min.js
  jquery.countdown.min.js jquery.ajaxchimp.min.js prepa_custom.js bootstrap-dialog.min.js
- jquery.easing.min.js register-init.js jquery.dataTables.min.js
+ jquery.easing.min.js register-init.js jquery.dataTables.min.js bootstrap_dialog4.js
 
  raphael-min.js sidebarmenu.js waves.js c3.min.css cryptocoins.css dashboard1.css flag-icon.min.css 
  font-awesome.min.css jquery.toast.css materialdesignicons.min.css morris.css simple-line-icons.css
  style.css themify-icons.css weather-icons.min.css
 
  bootstrap.min.css font-awesome.css themify-icons.css animate.css theme.css responsive.css
- owl.carousel.css nivo-lightbox.css owl.theme.css default.css bootstrap-dialog.min.css)
+ owl.carousel.css nivo-lightbox.css owl.theme.css default.css bootstrap-dialog.min.css bootstrap_dialog4.css)
