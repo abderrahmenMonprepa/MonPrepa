214ce1e5679895887199e3c4430b1ccc161105d6
Abderrahmen <abderrahmen@monprepa.tn>
2018-08-10 15:22:47 +0200

    Fixing Edit and New for passwords reset

--- app/helpers/devise_helper.rb
+++ app/helpers/devise_helper.rb
@@ -4,12 +4,9 @@ module DeviseHelper
 
     messages = resource.errors.full_messages.map { |msg| content_tag(:li, msg) }.join
     html = <<-HTML
-    <div class="alert alert-danger alert-dismissible fade show" role="alert">
-      <button type="button" class="close" data-dismiss="alert">
-        <span aria-hidden="true">&times;</span>
-      </button>
+    <div class="">
       <strong>
-       #{pluralize(resource.errors.count, "error")} must be fixed
+       #{pluralize(resource.errors.count, "erreur")} 
       </strong>
       #{messages}
     </div>
