31376bab7ba8e8db13aa83341ba9d3db4692e509
Abderrahmen <abderrahmen@monprepa.tn>
2018-08-18 15:09:57 +0200

    Email Sendgrid : Configuration not Accomplished

--- config/initializers/email_setup.rb
+++ config/initializers/email_setup.rb
@@ -1,10 +1,10 @@
-ActionMailer::Base.delivery_method = :smtp
-ActionMailer::Base.smtp_settings = {
-  address:              'smtp.sendgrid.net',
-  port:                 587,  # 465 for ssl connections
-  domain:               '37.187.29.92',
-  user_name:            Rails.application.secrets.sendgrid_username,
-  password:             Rails.application.secrets.sendgrid_password,
-  authentication:       'plain',
-  enable_starttls_auto: true 
-}
+# ActionMailer::Base.delivery_method = :smtp
+# ActionMailer::Base.smtp_settings = {
+#   address:              'smtp.sendgrid.net',
+#   port:                 587,  # 465 for ssl connections
+#   domain:               '37.187.29.92',
+#   user_name:            Rails.application.secrets.sendgrid_username,
+#   password:             Rails.application.secrets.sendgrid_password,
+#   authentication:       'plain',
+#   enable_starttls_auto: true 
+# }
